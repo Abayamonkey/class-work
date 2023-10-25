95bf54eeeda6304750343e727535a29654803316
Abayamonkey <145524013+Abayamonkey@users.noreply.github.com>
2023-10-25 04:17:02 +0000

    style

--- mind.js
+++ mind.js
@@ -1,5 +1,11 @@
-let myluckynumber=666;
-let octopusLimbs =69;
-console.log(myluckynumber, octopusLimbs);
-console.log("HHHHHHHHHHHHHHHHHHHHHHHH");
-console.log(myluckynumber, octopusLimbs);
+function login() {
+    let name =document.getElementById("ner").value;
+    let pass =document.getElementById("nuuts").value;
+    if(name == "admin" && pass =="123") {
+        alert("succes, now love ur self");
+    }else {
+        alert("ehh incorect pass or name u should kill ur self now!")
+    }
+
+}
+
